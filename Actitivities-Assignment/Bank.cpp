f3ffcb084fc5fe587d6ffa560de3090ef602b615
sSquishy <johnpaulopbattung@iskolarngbayan.pup.edu.ph>
2023-07-06 23:21:44 +0800

    Act-Ass update
    Act-Ass update 20

---
+++ Actitivities-Assignment/Bank.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>    
+int main()
+
+{
+	int savings = 1000, deposit, bankm;
+	
+	
+	bankm = deposit + savings;
+	
+	printf("Welcome to the Company's' Bank \n");
+	printf("Your current account is %d \n \n", savings);
+	
+	
+	printf("Enter the Amount to Deposit:");
+	scanf("%d", &deposit);
+	printf("Your total balance is %d", bankm);
+	
+	
+	
+	return 0;
+}
